e05d39045b37304767c380b78ef5e66194c884e1
Nikola Novak <reasonsharp@gmail.com>
2023-02-13 00:11:16 +0100

    Fix charset of sent mail

--- html/sendmessage.php
+++ html/sendmessage.php
@@ -14,7 +14,7 @@ $sig  = "-------------------\r\n$wsrv";
 
 $body = "Hi!\r\n\r\nA visitor filled out a contact form on $wsrv. Here are the details they entered:\r\n\r\nType: $type\r\nSender: $sndr\r\nMessage text:\r\n$text\r\n\r\n$sig\r\n";
 
-$mail_orig = "MIME_Version: 1.0\r\nContent-type: text/plain\r\nFrom: $from\r\nTo: $to\r\nSubject: ($wsrv) Contact form was filled out\r\nReturn-Path: $from\r\n\r\n$body\r\n";
+$mail_orig = "MIME_Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\nFrom: $from\r\nTo: $to\r\nSubject: ($wsrv) Contact form was filled out\r\nReturn-Path: $sndr\r\n\r\n$body\r\n";
 
 $file = fopen('/smtp-cli/message.txt', 'w') or die(file_get_contents("fail.html"));
 fwrite($file, $mail_orig);
